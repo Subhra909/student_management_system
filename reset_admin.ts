a44840b1da3ab681e2d7356e7d1c71616a26ba8e
Subhra909 <abirsanyal99@gmail.com>
2026-06-10 19:31:18 +0530

    minor changes

--- reset_admin.ts
+++ reset_admin.ts
@@ -5,19 +5,19 @@ import { connectDB, User } from "./src/server/db.js";
 async function run() {
   await connectDB();
   const email = "jis@gmail.com";
-  const password = "SecurePassword123!";
-  
+  const password = "jis@1234";
+
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  
+
   const result = await User.updateOne({ email }, { $set: { password: hashedPassword } });
-  
+
   if (result.matchedCount > 0) {
     console.log(`SUCCESS: Password for ${email} updated to ${password}`);
   } else {
     console.log(`ERROR: User ${email} not found.`);
   }
-  
+
   process.exit(0);
 }
 
